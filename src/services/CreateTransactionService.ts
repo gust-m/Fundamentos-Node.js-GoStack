42f0d6ec7785f37f59f2e021973c8aa5f243f7bd
Gustavo <gustavoc.moraes@hotmail.com>
2021-01-21 21:46:31 -0300

    Validation transaction data

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,42 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Transaction type invalid');
+    }
+
+    if (type === 'outcome') {
+      const validationOutcome = total - value;
+
+      if (validationOutcome < 0) {
+        throw Error('You have no enough money');
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
